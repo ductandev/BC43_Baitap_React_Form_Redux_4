5a1c71b7526cc1197bc8769ae7d3047962db9f47
Nguyen Duc Tan <nguyenductan998@gmail.com>
2023-05-17 15:38:42 +0700

    update_code

--- src/redux/reducers/sinhVienReducer.js
+++ src/redux/reducers/sinhVienReducer.js
@@ -170,15 +170,12 @@ const sinhVienReducer = createSlice({
         for (var i=0, l=from.length ; i < l ; i++) {
           str = str.replace(RegExp(from[i], "gi"), to[i]);
         }
-      
         str = str.toLowerCase()
               .trim()
               .replace(/[^a-z0-9]/g, '-')
               .replace(/-+/g, '-');
-      
         return str;
       }
-
 
 
       let { value } = action.payload;
